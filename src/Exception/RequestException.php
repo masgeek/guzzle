00835016a46ed3de9632cc7053068058101936b4
Sammy M <barsamms@gmail.com>
2018-09-19 13:35:45 +0300

    REverted to className()
    Scheme

--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -81,10 +81,10 @@ class RequestException extends TransferException
         $level = (int) floor($response->getStatusCode() / 100);
         if ($level === 4) {
             $label = 'Client error';
-            $className = ClientException::class;
+            $className = ClientException::className();
         } elseif ($level === 5) {
             $label = 'Server error';
-            $className = ServerException::class;
+            $className = ServerException::className();
         } else {
             $label = 'Unsuccessful request';
             $className = __CLASS__;
